19a70a949d0f70650116e11bff3bb7a8fe77e743
arthomas07 <arthur.thomas@etu.minesparis.psl.eu>
2023-11-09 12:05:54 +0100

    Cleaning + new journey
    - On pack dans des fonctions (solve and plot, calc, init, compute PL...)
    - Ajout de l'option series : va chercher QF_out et alpha dans le dict si True, les set à 0 et 1 respectivement sinon

--- data_plots.py
+++ data_plots.py
@@ -3,47 +3,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import model_fsolve as modf
+import from_excel as fe
 
 ### Pressure losses plots 
 def PL_percent(PL):
     PL_per = pd.DataFrame((list(zip(PL["Total PL"], 100*PL["SPL entrance"]/PL["Total PL"], 100*PL["RPL manifold"]/PL["Total PL"], 100*PL["RPL riser"]/PL["Total PL"], 100*PL["SPL tee"]/PL["Total PL"]))), columns = ["Total PL", "SPL entrance", "RPL manifold", "RPL riser", "SPL tee"])
-    PL_per.reset_index().plot(x='index', y= ["SPL entrance", "RPL manifold", "RPL riser", "SPL tee"], style='o', xlabel='N° riser', ylabel='% head loss contribution')
+    PL_per.plot(y= ["SPL entrance", "RPL manifold", "RPL riser", "SPL tee"], xlabel='N° riser', ylabel='% head loss contribution')
 
 def PL_hist(PL):
-    PL.plot.bar(y=['SPL entrance', 'RPL manifold', 'RPL riser', 'SPL tee', 'Pressure recovery'], stacked = True, xlabel='N° riser', ylabel='Pressure loss')
+    PL.plot.bar(y=['SPL entrance', 'RPL manifold', 'RPL riser', 'SPL tee', 'Pressure recovery'], stacked = True, xlabel='N° riser', ylabel='Pressure loss [Pa]', xticks= np.linspace(0, len(PL), 20))
 
-def PL_dv(par, condi0, list_Vdot, lab=None):
-    list_Dv = list_Vdot/3600000 # m3/s
-    list_PL,list_tabl,list_mn,list_std = modf.PL_fsolve_range(par,condi0,list_Dv)
-    plt.plot(list_Vdot, list_PL/1000,'-o', label=lab)
-    plt.xlabel('Flow rate [L/h]')
+def PL_dv(list_Vdot, list_PL, lab=None, unit = None):
+    if unit == 'L/min':
+        plt.plot(list_Vdot/60, list_PL/1000,'-o', label=lab)
+        plt.xlabel('Flow rate [L/min]')
+    else :
+        plt.plot(list_Vdot, list_PL/1000,'-o', label=lab)
+        plt.xlabel('Flow rate [L/h]')
     plt.ylabel('Pressure loss [kPa]')
     plt.legend()
-    return(list_PL, list_tabl, list_mn, list_std)
 
-def PL_ratio(par, condi0, list_Vdot, list_rd):
+def PL_ratio(par, cond, list_Vdot, list_rd, unit=None):
+    D0_riser = par['D_riser']
+    D0_man = par['D_man']
+    r0 = D0_riser/D0_man
     for rd in list_rd:
-        modf.change_diameter(par, par['D_riser']/rd, name='man')
-        PL_dv(par,condi0,list_Vdot, lab=f'Rd = {rd}')
+        modf.change_diameter(par, D0_man*np.sqrt(r0/rd), name='man')
+        modf.change_diameter(par, D0_riser*np.sqrt(rd/r0), name='riser')
+        list_PL, list_tabl = modf.PL_fsolve_range(par, cond, np.array(list_Vdot)/3600000)
+        PL_dv(list_Vdot, list_PL, lab=f'Rd = {rd}', unit=unit)
 
 ### Flow distribution plots
 
-def beta_riser(tabl, axis=None, lab=None):
+def beta_riser(tabl, axis=None, lab=None, dimensionless=True, rho=None):
     list_qx = tabl['qx'][:]
     N = len(list_qx)
     list_qx.reset_index(drop=True,inplace=True)
+    if dimensionless:
+        list_beta = list_qx/list_qx.mean()
+        plt.plot([i for i in range(N)], list_beta, label=lab)
+        plt.axis(axis)
+        plt.ylim(ymin=0,ymax=max(list_beta)+0.2)
+        plt.xlabel('N° riser')
+        plt.ylabel('q_x / q_moy')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),ncol=2)
+    else :
+        plt.plot([i for i in range(N)], list_qx*rho/3600000, label=lab)
+        plt.axis(axis)
+        plt.ylim(ymin=0)
+        plt.xlabel('N° riser')
+        plt.ylabel('Mass flow rate [kg/s]')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),ncol=2)    
 
-    list_beta = list_qx/list_qx.sum()
+def beta_rd(list_rd, list_tabl):
+    for i in range(len(list_tabl)):
+        beta_riser(list_tabl[i], lab=f'rd = {list_rd[i]} L/h')
 
-    plt.plot([i for i in range(N)], list_beta, '-o', label=lab)
-    plt.axis(axis)
-    plt.xlabel('N° riser')
-    plt.ylabel('Beta_i')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),ncol=2)
-
-def beta_vdot(par,condi0,list_Vdot):
-    list_Dv = list_Vdot/3600000 # m3/s
-    list_PL,list_tabl,list_mn,list_std = modf.PL_fsolve_range(par,condi0,list_Dv)
+def beta_vdot(list_Vdot, list_tabl):
     for i in range(len(list_tabl)):
         beta_riser(list_tabl[i], lab=f'flow rate = {list_Vdot[i]: .1f} L/h')
 
@@ -76,3 +92,30 @@ def K_rd(par, condi0, list_rd, name = 'Kx_in',lab=None):
     plt.ylabel(f'{name}')
     plt.legend()
     plt.show()
+
+
+### SOLVE AND PLOT
+
+def solve_plot(path, file_name, hist_pl = True, perc_pl = True, pl_dv = True, beta_dv = True, list_Vdot = np.array([50, 200, 350, 500]), series = False):
+    
+    hx, par, condi0 = fe.initialize(path, file_name)
+
+    tabl, res, PL = modf.PL_fsolve(par,condi0, show=False, series=series)
+    if hist_pl :
+        PL_hist(PL)
+        plt.show()
+    if perc_pl :
+        PL_percent(PL)
+        plt.show()
+
+    if pl_dv or beta_dv :
+        list_PL, list_tabl= modf.PL_fsolve_range(par, condi0, np.array(list_Vdot)/3600000)     
+
+        if pl_dv :
+            PL_dv(list_Vdot, list_PL)
+            plt.show()
+        if beta_dv :
+            beta_vdot(list_Vdot, list_tabl)
+            plt.show()
+
+    PL_ratio(par, condi0, list_Vdot, [0.25, 0.5, 1.], unit=None)
